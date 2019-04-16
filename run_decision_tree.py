97f86fd986d8e7dd88bf8a822bf1a2b7372b3db3
Chungsang Tom Lam <erinata@gmail.com>
2019-04-16 16:42:35 -0400

    fit model and generate confusion matrix

--- run_decision_tree.py
+++ run_decision_tree.py
@@ -1,6 +1,9 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn import tree
 
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
 
 dataset = pd.read_csv("dataset.csv")
 
@@ -19,6 +22,20 @@ print(data_training.head())
 print("data_test")
 print(data_test.head())
 
+decision_tree_machine = tree.DecisionTreeClassifier(criterion="gini")
+decision_tree_machine.fit(data_training, target_training)
+
+predictions = decision_tree_machine.predict(data_test)
+
+print(accuracy_score(target_test, predictions))
+
+confusion_matrix = pd.DataFrame(
+	confusion_matrix(target_test,predictions),
+	columns = ['Predict 0', 'Predict 1', 'Predict 2', 'Predict 3'],
+	index = ['True 0', 'True 1', 'True 2', 'True 3']
+)
+
+print(confusion_matrix)
 
 
 
